f00638551f11f45b8dd05358c77db7108e6df46e
Phoebe Lei Yuhe <39511157+ylei003@users.noreply.github.com>
2022-09-27 22:20:03 +0800

    Add files via upload

---
+++ spyder/print_integers.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 27 21:45:04 2022
+
+@author: Lei Yuhe
+"""
+
+for i in range(10):
+    print(i)
+    
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
